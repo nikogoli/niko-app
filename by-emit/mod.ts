40f33aaf71813f14a9ed8eea1781080a6db43c07
nikogoli <k_m_naoto@yahoo.co.jp>
2023-11-06 12:51:25 +0900

    変更：singal 系を通常の export に

--- by-emit/mod.ts
+++ by-emit/mod.ts
@@ -3,7 +3,11 @@ export * as Gluon from "../gluon_deno/index.js"
 
 export { setHTML, type ViewConfig, type SetViewProps } from "./setHTML.tsx"
 
-export { signal, type Signal } from "../utils/signals.js"
+export {
+  signal, batch, computed, effect,
+  useComputed, useSignal, useSignalEffect,
+  type Signal, type ReadonlySignal
+} from "https://esm.sh/@preact/signals@1.1.3?deps=preact@10.15.1"
 export {
   serve, serveStatic,
   siftLog, timeKeeper,
