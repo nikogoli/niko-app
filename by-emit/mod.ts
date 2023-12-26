b2b797e61d892c91355cf544c43d7caa84ce2bc3
nikogoli <k_m_naoto@yahoo.co.jp>
2023-12-26 19:37:40 +0900

    削除：signal 関連の import

--- by-emit/mod.ts
+++ by-emit/mod.ts
@@ -4,11 +4,6 @@ export * as Gluon from "../gluon_deno/index.js"
 export { setHTML, type ViewConfig, type SetViewProps } from "./setHTML.tsx"
 
 export {
-  signal, batch, computed, effect,
-  useComputed, useSignal, useSignalEffect,
-  type Signal, type ReadonlySignal
-} from "https://esm.sh/@preact/signals@1.1.3?deps=preact@10.15.1"
-export {
   serve, serveStatic,
   siftLog, timeKeeper,
   blue, green, red, yellow, cyan, magenta,
