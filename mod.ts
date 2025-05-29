5c46d21751d20b893de7c048d7c2bcfd093a7d15
nikogoli <k_m_naoto@yahoo.co.jp>
2025-05-30 00:32:14 +0900

    変更：mod.tsx をルート直下に移動

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
-export { setHTML, type ViewConfig, type SetViewProps } from "./setHTML.tsx"
+export { setHTML, type ViewConfig, type SetViewProps } from "./by-esbuild/setHTML.tsx"
 
 export {
   serve, serveStatic,
   siftLog, timeKeeper,
   blue, green, red, yellow, cyan, magenta,
   type Handler, type Routes, type ServeStaticOptions,
-} from "../utils/niko-router.ts"
+} from "./utils/niko-router.ts"
