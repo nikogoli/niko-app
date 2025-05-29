42a135b34708978ad991fe1bf137c7942da9c57d
nikogoli <k_m_naoto@yahoo.co.jp>
2025-05-30 00:31:07 +0900

    削除：by-emit

--- by-emit/mod.ts
+++
@@ -1,8 +0,0 @@
-export { setHTML, type ViewConfig, type SetViewProps } from "./setHTML.tsx"
-
-export {
-  serve, serveStatic,
-  siftLog, timeKeeper,
-  blue, green, red, yellow, cyan, magenta,
-  type Handler, type Routes, type ServeStaticOptions,
-} from "../utils/niko-router.ts"
